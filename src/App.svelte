1d882ff326319445fea5fcd16e49f0cdbd7b51c3
neilprabhu <neilprabhu96@gmail.com>
2025-02-03 13:51:22 -0800

    sound working

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
 	import HowTo from './HowTo.svelte'
 	import Timer from'./Timer.svelte'
+	let audio;
+	function timerEnds(event) {
+		console.log(event);
+		audio.play();
+	}
 </script>
 
 <style>
@@ -13,7 +18,7 @@
 
 <h1> Handwashing App </h1>
 
-<Timer />
+<Timer on:end={timerEnds} />
 
 <HowTo />
 
@@ -21,3 +26,7 @@
     <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
     <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
 </h3>
+
+<audio bind:this={audio}>
+	<source src="sound.wav"/>
+</audio>
